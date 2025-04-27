208a4cf261fe0a2780bae9184ba0504bc5c2a743
Rafael Castro <rcastrometa@gmail.com>
2025-04-27 18:52:26 +0100

    Revamp v0.0.7

--- novastra-nextjs/components/sections/CaseStudies.tsx
+++ novastra-nextjs/components/sections/CaseStudies.tsx
@@ -340,7 +340,7 @@ const CaseStudies: React.FC = () => {
         backgroundColor: 'rgba(0, 0, 0, 0.8)',
         titleFont: {
           size: 16,
-          weight: 'normal'
+          weight: 'bold'
         },
         bodyFont: {
           size: 14
